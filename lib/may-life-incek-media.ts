9fd470fbc13502576f7195765628db56596ff678
BBmyproject <bedirhannugurlu@gmail.com>
2026-04-28 19:02:28 +0300

    May incek photo

--- lib/may-life-incek-media.ts
+++ lib/may-life-incek-media.ts
@@ -1,6 +1,6 @@
 export const MAY_LIFE_INCEK_MEDIA = {
-  concept: "/images/may-life-incek.jpg",
-  architecture: "/images/may-life-ince-2.jpg",
-  social: "/images/may-life-incek-4.jpg",
-  location: "/images/may-life-incek-3.jpg",
+  concept: "/images/may-life-incek-4.jpg",
+  architecture: "/images/may-life-incek-2.jpg",
+  social: "/images/may-life-incek-3.jpg",
+  location: "/images/may-life-incek-5.jpg",
 } as const;
